228c3f614d6951e2760256f89c488362ba6d29ce
opl224 <siapaajaboleh202@gmail.com>
2024-07-15 07:15:01 +0700

    frontend

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,1 +1,21 @@
-s;
+import Message from "./Message";
+
+const Messages = () => {
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
+};
+export default Messages;
